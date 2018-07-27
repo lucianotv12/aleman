09884edf25eb9f64838085eae9e8aaa8d086422c
lucianotv12 <lucianotv12@gmail.com>
2018-07-26 23:16:09 -0300

    s

--- admin/view/productos/producto_abm.php
+++ admin/view/productos/producto_abm.php
@@ -9,7 +9,7 @@
                                 <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
                                 <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
                             </div>
-                            <h4 class="panel-title">Producto : </h4>
+                            <h4 class="panel-title">Producto : <?php echo @$producto->descripcion?></h4>
                         </div>
                         <div class="panel-body">
   							<?php if($cambio == "new"):?>
@@ -24,7 +24,7 @@
                                    <input name="_idProducto" type="hidden" value="<?php echo $_GET['id'];?>">
                                 <?php endif;?>   
                             <?php endif;?>	
-                      <!--          <div class="form-group">
+                                <div class="form-group">
                                     <label class="col-md-3 control-label">Categorias</label>
                                     <div class="col-md-9">
                                         <select name="idCategoria" class="form-control">
@@ -44,7 +44,7 @@
 
                                         </select>
                                     </div>
-                                </div>                         -->       
+                                </div>                                
                                 <div class="form-group">
                                     <label class="col-md-3 control-label">Nombre</label>
                                     <div class="col-md-9">
@@ -150,41 +150,6 @@
 		
 
 	</div>
-	<!-- end page container -->
-	<!-- ================== BEGIN BASE JS ================== -->
-	<script src="<?php echo ADMIN?>assets/plugins/jquery/jquery-1.9.1.min.js"></script>
-	<script src="<?php echo ADMIN?>assets/plugins/jquery/jquery-migrate-1.1.0.min.js"></script>
-	<script src="<?php echo ADMIN?>assets/plugins/jquery-ui/ui/minified/jquery-ui.min.js"></script>
-	<script src="<?php echo ADMIN?>assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-	<!--[if lt IE 9]>
-		<script src="assets/crossbrowserjs/html5shiv.js"></script>
-		<script src="assets/crossbrowserjs/respond.min.js"></script>
-		<script src="assets/crossbrowserjs/excanvas.min.js"></script>
-	<![endif]-->
-	<script src="<?php echo ADMIN?>assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-	<script src="<?php echo ADMIN?>assets/plugins/jquery-cookie/jquery.cookie.js"></script>
-	<!-- ================== END BASE JS ================== -->
-	
-	<!-- ================== BEGIN PAGE LEVEL JS ================== -->
-    <script src="<?php echo ADMIN?>assets/plugins/ckeditor/ckeditor.js"></script>
-    <script src="<?php echo ADMIN?>assets/plugins/bootstrap-wysihtml5/lib/js/wysihtml5-0.3.0.js"></script>
-    <script src="<?php echo ADMIN?>assets/plugins/bootstrap-wysihtml5/src/bootstrap-wysihtml5.js"></script>
-    <script src="<?php echo ADMIN?>assets/js/form-wysiwyg.demo.min.js"></script>
-    <script src="<?php echo ADMIN?>assets/js/apps.min.js"></script>    
-	<script src="<?php echo ADMIN?>assets/plugins/DataTables/media/js/jquery.dataTables.js"></script>
-	<script src="<?php echo ADMIN?>assets/plugins/DataTables/media/js/dataTables.bootstrap.min.js"></script>
-	<script src="<?php echo ADMIN?>assets/plugins/DataTables/extensions/Buttons/js/dataTables.buttons.min.js"></script>
-	<script src="<?php echo ADMIN?>assets/plugins/DataTables/extensions/Buttons/js/buttons.bootstrap.min.js"></script>
-	<script src="<?php echo ADMIN?>assets/plugins/DataTables/extensions/Buttons/js/buttons.flash.min.js"></script>
-	<script src="<?php echo ADMIN?>assets/plugins/DataTables/extensions/Buttons/js/jszip.min.js"></script>
-	<script src="<?php echo ADMIN?>assets/plugins/DataTables/extensions/Buttons/js/pdfmake.min.js"></script>
-	<script src="<?php echo ADMIN?>assets/plugins/DataTables/extensions/Buttons/js/vfs_fonts.min.js"></script>
-	<script src="<?php echo ADMIN?>assets/plugins/DataTables/extensions/Buttons/js/buttons.html5.min.js"></script>
-	<script src="<?php echo ADMIN?>assets/plugins/DataTables/extensions/Buttons/js/buttons.print.min.js"></script>
-	<script src="<?php echo ADMIN?>assets/plugins/DataTables/extensions/Responsive/js/dataTables.responsive.min.js"></script>
-	<script src="<?php echo ADMIN?>assets/js/table-manage-buttons.demo.min.js"></script>
-	<script src="<?php echo ADMIN?>assets/js/apps.min.js"></script>
-	<!-- ================== END PAGE LEVEL JS ================== -->
 	
 
 
