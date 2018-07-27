caf099c4c3a5b2a370fdf894137d12710d01cb99
lucianotv12 <lucianotv12@gmail.com>
2018-07-26 23:08:06 -0300

    s

--- admin/ctrl/index.php
+++ admin/ctrl/index.php
@@ -79,9 +79,10 @@ switch($accion):
         $iva_10="";
 		$categorias = $producto->get_categorias_combo();
 		$monedas = $producto->get_monedas();
-		echo "aca todo bien";die;
 
 		Template::draw_header(2, 'productos');
+		echo "aca todo bien";die;
+
 		include("../view/productos/producto_abm.php");
 		}
 		break;	
