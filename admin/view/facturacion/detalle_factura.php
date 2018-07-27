5c7c0771b76a291ff7f6d31fc9afddd42701e827
lucianotv12 <lucianotv12@gmail.com>
2018-07-27 12:27:18 -0300

    s

--- admin/view/facturacion/detalle_factura.php
+++ admin/view/facturacion/detalle_factura.php
@@ -1,83 +1,67 @@
 
 			<!-- begin invoice -->
 			<div class="invoice">
-               <div class="invoice-company">
+                <div class="invoice-company">
                     <span class="pull-right hidden-print">
-                    <a href="javascript:;" class="btn btn-sm btn-success m-b-10"><i class="fa fa-download m-r-5"></i> Export as PDF</a>
-                    <a href="javascript:;" onclick="window.print()" class="btn btn-sm btn-success m-b-10"><i class="fa fa-print m-r-5"></i> Print</a>
+                    <a href="javascript:;" class="btn btn-sm btn-success m-b-10"><i class="fa fa-download m-r-5"></i> Exportar a PDF</a>
+                    <a href="javascript:;" onclick="window.print()" class="btn btn-sm btn-success m-b-10"><i class="fa fa-print m-r-5"></i> Imprimir</a>
                     </span>
-                    Company Name, Inc
+                    Maderas "El Aleman"
                 </div>
                 <div class="invoice-header">
                     <div class="invoice-from">
-                        <small>from</small>
+                        <small>De</small>
                         <address class="m-t-5 m-b-5">
-                            <strong>Twitter, Inc.</strong><br />
-                            Street Address<br />
-                            City, Zip Code<br />
-                            Phone: (123) 456-7890<br />
+                            <strong>Maderas "El Aleman"</strong><br />
+                            Direccion<br />
+                            General Rodriguez, Buenos Aires<br />
+                            Telefono: (123) 456-7890<br />
                             Fax: (123) 456-7890
                         </address>
                     </div>
                     <div class="invoice-to">
-                        <small>to</small>
+                        <small>Para</small>
                         <address class="m-t-5 m-b-5">
-                            <strong>Company Name</strong><br />
-                            Street Address<br />
-                            City, Zip Code<br />
-                            Phone: (123) 456-7890<br />
-                            Fax: (123) 456-7890
+                            <strong>Consumidor Final</strong><br />
+                            domicilio<br />
+                            Ciudad<br />
+                            Telefono: (123) 456-7890<br />
                         </address>
                     </div>
                     <div class="invoice-date">
-                        <small>Invoice / July period</small>
-                        <div class="date m-t-5">August 3,2012</div>
+                        <small>Remito X / <?php echo $factura["id"]?></small>
+                        <div class="date m-t-5"><?php echo date("d/m/Y");?></div>
                         <div class="invoice-detail">
                             #0000123DSS<br />
-                            Services Product
+                            Productos varios
                         </div>
                     </div>
                 </div>
                 <div class="invoice-content">
+
+                    <form name="datos" method="post" enctype="multipart/form-data" action="<?php echo HOME?>generar_factura.html" onKeyPress="return disableEnterKey(event)" >
+
                     <div class="table-responsive">
-                        <table class="table table-invoice">
-                            <thead>
-                                <tr>
-                                    <th>TASK DESCRIPTION</th>
-                                    <th>RATE</th>
-                                    <th>HOURS</th>
-                                    <th>LINE TOTAL</th>
+                        <table id="mitabla" class="table table-invoice" >
+                                <tr id="0">
+                                    <th>Cant.</th>
+                                    <th>Art.</th>
+                                    <th >Detalle</th>
+                                    <th>P/unitario</th>
+                                    <th>Desc.</th>
+                                    <th>Importe</th>
+                                    <th></th>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        Website design &amp; development<br />
-                                        <small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id sagittis arcu.</small>
-                                    </td>
-                                    <td>$50.00</td>
-                                    <td>50</td>
-                                    <td>$2,500.00</td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        Branding<br />
-                                        <small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id sagittis arcu.</small>
-                                    </td>
-                                    <td>$50.00</td>
-                                    <td>40</td>
-                                    <td>$2,000.00</td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        Redesign Service<br />
-                                        <small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id sagittis arcu.</small>
-                                    </td>
-                                    <td>$50.00</td>
-                                    <td>50</td>
-                                    <td>$2,500.00</td>
-                                </tr>
-                            </tbody>
+                                <?php foreach($productos as $producto):?>
+                                    <tr>
+                                        <td><?php echo $producto["cantidad"]?></td>
+                                        <td><?php echo $producto["idProducto"]?></td>
+                                        <td><?php echo $producto["descripcion"]?></td>
+                                        <td><?php echo $producto["precio_unitario"]?></td>
+                                        <td><?php echo $producto["descuento"]?></td>
+                                        <td><?php echo $producto["precio_total"]?></td>
+                                    </tr>    
+                                <?php endforeach;?>    
                         </table>
                     </div>
                     <div class="invoice-price">
@@ -85,23 +69,24 @@
                             <div class="invoice-price-row">
                                 <div class="sub-price">
                                     <small>SUBTOTAL</small>
-                                    $4,500.00
+                                    $<span id="subtotal_final"><?php echo $factura["importe"]?></span>
                                 </div>
                                 <div class="sub-price">
                                     <i class="fa fa-plus"></i>
                                 </div>
                                 <div class="sub-price">
-                                    <small>PAYPAL FEE (5.4%)</small>
-                                    $108.00
+                                    <small>Iva (21%)</small>
+                                    $0
                                 </div>
                             </div>
                         </div>
                         <div class="invoice-price-right">
-                            <small>TOTAL</small> $4508.00
+                            <small>TOTAL</small> 
+                            $<span id="total_final"><?php echo $factura["importe"]?></span>
                         </div>
                     </div>
                 </div>
-<!--                <div class="invoice-note">
+                <div class="invoice-note">
                     * Nota 1<br />
                     * Nota 2<br />
                     * Nota 3
@@ -115,7 +100,8 @@
                         <span class="m-r-10"><i class="fa fa-phone"></i> T:0237-4444444</span>
                         <span class="m-r-10"><i class="fa fa-envelope"></i> elaleman@gmail.com</span>
                     </p>
-                </div>-->
+                </div>
+
 
 
                 </form>
