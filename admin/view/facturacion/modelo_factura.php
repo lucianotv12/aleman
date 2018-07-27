18b5c932f5ee5df48386801beea8033fa716e9f0
lucianotv12 <lucianotv12@gmail.com>
2018-07-27 00:34:34 -0300

    s

--- admin/view/facturacion/modelo_factura.php
+++ admin/view/facturacion/modelo_factura.php
@@ -40,7 +40,7 @@
                 <div class="" style="background: #5DBD90;padding: 20px; color: white">
                         <div class="panel-body panel-form">
                             <form class="form-horizontal form-bordered">
-                                <div class="form-group">
+                                <div class="form-group" style="color: white">
                                     <label class="control-label col-md-2">Buscar Productos</label>
                                     <div class="col-md-10">
                                         <input type="text" name="" id="jquery-autocomplete" class="form-control" placeholder="busqueda inteligente" />
