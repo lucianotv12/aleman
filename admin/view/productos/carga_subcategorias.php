330783fef6bccb87cf8cad2ecdd1b652985c2adb
Ibris <lucianotv12@gmail.com>
2018-08-01 15:44:50 -0300

    s

--- admin/view/productos/carga_subcategorias.php
+++ admin/view/productos/carga_subcategorias.php
@@ -6,15 +6,7 @@ include_once("../../funciones.php");
 //$_GET["code"];
 $categoria = $_GET["code"];
 
-if($_GET["code"] != "0"):
-	$subcategorias = Producto::get_subcategoria_byid($categoria);
-	
-?>     
-<?php foreach($subcategorias as $subcategoria): ?>
-		<option value="<?php echo $subcategoria["id"];?>"><?php echo $subcategoria["nombre"];?></option>	
-<?php endforeach;
 
-endif;
 ?>
 
 
