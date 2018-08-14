1a8b2a468d584f499ba0468e7d14ff94edfcf098
Ibris <lucianotv12@gmail.com>
2018-08-14 16:59:56 -0300

    s

--- admin/view/productos/producto_stock.php
+++ admin/view/productos/producto_stock.php
@@ -39,7 +39,8 @@
                                 </tbody>
                             </table>
 
-  			
+
+		  			
                         </div>
                     </div>
                     <!-- end panel -->
