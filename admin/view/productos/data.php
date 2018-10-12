cb62c87c66828f6edb5c1f462d62b57528a679ca
lucianotv12 <lucianotv12@gmail.com>
2018-10-12 10:57:26 -0300

    a

--- admin/view/productos/data.php
+++ admin/view/productos/data.php
@@ -4,12 +4,7 @@ include_once("../../funciones.php");
 
 $productos = Producto::get_productos();
 
-$results = array(
-"sEcho" => 1,
-"iTotalRecords" => count($productos),
-"iTotalDisplayRecords" => count($productos),
-"aaData"=>$productos);
-echo json_encode($results);
+
 
 
 ?>
