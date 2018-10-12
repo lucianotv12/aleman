7cfa7158b70022aa2bcb828c4ef7f3c7f278bd07
lucianotv12 <lucianotv12@gmail.com>
2018-10-12 17:25:53 -0300

    s

--- admin/view/productos/productos.php
+++ admin/view/productos/productos.php
@@ -28,7 +28,7 @@
 									<form method="post" name="datos" action="<?php echo HOME?>home.html">
                                 	<th  colspan="9">BUSCAR <input type="text" size="70" name="buscador" id="buscar_usuarios" value="<?php echo  $_POST["buscador"]?>" >
 									<b>
-									<button type="submit" style="color:#5DBD90" onmouseover="this.style.color='blue'" onmouseout="this.style.color='white'" href="javaScript:busqueda('list','<?php echo  $_POST['buscador'] ?>')">BUSCAR</button>
+									<button type="submit" style="color:#5DBD90" onmouseover="this.style.color='blue'" onmouseout="this.style.color='white'">BUSCAR</button>
 
 									<a style="color:#5DBD90" onmouseover="this.style.color='blue'" onmouseout="this.style.color='white'" href="javaScript:busqueda('list','TODOS')">TODOS</a>	
 									</b></th>	
