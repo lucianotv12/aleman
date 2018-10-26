b7e44a5704e2576e7b1359461e70058ef6cb3dc1
lucianotv12 <lucianotv12@gmail.com>
2018-10-26 12:04:16 -0300

    s

--- admin/view/productos.php
+++ admin/view/productos.php
@@ -20,7 +20,7 @@
                             </button>
                         </div>
                         <div class="panel-body">
-                            <table id="data-table" class="table table-striped table-bordered">
+                            <table  class="table table-striped table-bordered">
                                 <thead>
 						          <?php if($_usuario->gerarquia == 1): ?>
 
@@ -98,25 +98,16 @@
 	<!-- ================== END BASE JS ================== -->
 	
 	<!-- ================== BEGIN PAGE LEVEL JS ================== -->
-	<script src="<?php echo HOME?>assets/plugins/DataTables/media/js/jquery.dataTables.js"></script>
-	<script src="<?php echo HOME?>assets/plugins/DataTables/media/js/dataTables.bootstrap.min.js"></script>
-	<script src="<?php echo HOME?>assets/plugins/DataTables/extensions/Buttons/js/dataTables.buttons.min.js"></script>
-	<script src="<?php echo HOME?>assets/plugins/DataTables/extensions/Buttons/js/buttons.bootstrap.min.js"></script>
-	<script src="<?php echo HOME?>assets/plugins/DataTables/extensions/Buttons/js/buttons.flash.min.js"></script>
-	<script src="<?php echo HOME?>assets/plugins/DataTables/extensions/Buttons/js/jszip.min.js"></script>
-	<script src="<?php echo HOME?>assets/plugins/DataTables/extensions/Buttons/js/pdfmake.min.js"></script>
-	<script src="<?php echo HOME?>assets/plugins/DataTables/extensions/Buttons/js/vfs_fonts.min.js"></script>
-	<script src="<?php echo HOME?>assets/plugins/DataTables/extensions/Buttons/js/buttons.html5.min.js"></script>
-	<script src="<?php echo HOME?>assets/plugins/DataTables/extensions/Buttons/js/buttons.print.min.js"></script>
-	<script src="<?php echo HOME?>assets/plugins/DataTables/extensions/Responsive/js/dataTables.responsive.min.js"></script>
-	<script src="<?php echo HOME?>assets/js/table-manage-buttons.demo.min.js"></script>
+
+
+
 	<script src="<?php echo HOME?>assets/js/apps.min.js"></script>
 	<!-- ================== END PAGE LEVEL JS ================== -->
 	
 	<script>
 		$(document).ready(function() {
 			App.init();
-			TableManageButtons.init();
+
 		});
 	</script>
 </body>
