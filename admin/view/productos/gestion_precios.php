f353a31dc40b1cb56de907bbcb273be699a51336
lucianotv12 <lucianotv12@gmail.com>
2018-10-26 14:19:10 -0300

    s

--- admin/view/productos/gestion_precios.php
+++ admin/view/productos/gestion_precios.php
@@ -200,3 +200,28 @@ $categorias= Producto::get_categorias_combo();
 </div>
 </div>
 </div></div>
+	<!-- end page container -->
+	<!-- ================== BEGIN BASE JS ================== -->
+	<script src="<?php echo HOME?>assets/plugins/jquery/jquery-migrate-1.1.0.min.js"></script>
+	<script src="<?php echo HOME?>assets/plugins/jquery-ui/ui/minified/jquery-ui.min.js"></script>
+	<script src="<?php echo HOME?>assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+	<!--[if lt IE 9]>
+		<script src="assets/crossbrowserjs/html5shiv.js"></script>
+		<script src="assets/crossbrowserjs/respond.min.js"></script>
+		<script src="assets/crossbrowserjs/excanvas.min.js"></script>
+	<![endif]-->
+	<script src="<?php echo HOME?>assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+	<script src="<?php echo HOME?>assets/plugins/jquery-cookie/jquery.cookie.js"></script>
+	<!-- ================== END BASE JS ================== -->
+	
+	<!-- ================== BEGIN PAGE LEVEL JS ================== -->
+
+
+	<script src="<?php echo HOME?>assets/js/apps.min.js"></script>
+	<!-- ================== END PAGE LEVEL JS ================== -->
+	
+	<script>
+		$(document).ready(function() {
+			App.init();
+	//		TableManageButtons.init();
+		});</script>
